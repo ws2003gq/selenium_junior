a5cb626c499e440c8e0b638ccf5e90fa5216f229
wangsheng <111-god@163.com>
2016-02-16 14:24:28 +0800

    first

---
+++ src/selenium_junior/Test1.java
@@ -0,0 +1,72 @@
+/**
+  * @FileName: Test1.java
+  * @Author 
+  * @Description:
+  * @Date 2016年2月16日 下午2:23:56
+  * @CopyRight CNP Corporation
+  */
+package selenium_junior;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/** 
+ * @FileName: Test1.java
+ * @author:  ws
+ * @Description: 
+ * @date: 2016年2月16日 下午2:23:56 
+ * @CopyRight :
+ */
+public class Test1{
+
+    /** 
+     * @Title: setUpBeforeClass 
+     * @Description: TODO(这里用一句话描述这个方法的作用) 
+     * @param @throws java.lang.Exception    设定文件 
+     * @return void    返回类型 
+     * @throws 
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception{}
+
+    /** 
+     * @Title: tearDownAfterClass 
+     * @Description: TODO(这里用一句话描述这个方法的作用) 
+     * @param @throws java.lang.Exception    设定文件 
+     * @return void    返回类型 
+     * @throws 
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception{}
+
+    /** 
+     * @Title: setUp 
+     * @Description: TODO(这里用一句话描述这个方法的作用) 
+     * @param @throws java.lang.Exception    设定文件 
+     * @return void    返回类型 
+     * @throws 
+     */
+    @Before
+    public void setUp() throws Exception{}
+
+    /** 
+     * @Title: tearDown 
+     * @Description: TODO(这里用一句话描述这个方法的作用) 
+     * @param @throws java.lang.Exception    设定文件 
+     * @return void    返回类型 
+     * @throws 
+     */
+    @After
+    public void tearDown() throws Exception{}
+
+    @Test
+    public void test(){
+        fail("Not yet implemented");
+    }
+
+}
